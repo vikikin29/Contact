259314b849ae8eaec2f83e6e97b797dd5e656ee9
vikicklin <vikicklin@gmail.com>
2017-11-23 10:01:07 +0100

    Přidejte soubory projektu.

---
+++ Kontakty/Kontakty/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kontakty
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
